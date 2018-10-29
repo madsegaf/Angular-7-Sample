1713935d0a078a8acd2df99fa86d6391fa692735
madsegaf <madsegaf44@live.com>
2018-10-29 17:00:58 +0700

    form validation with Model

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { DataService } from '../data.service';
+import { Kontak } from '../kontak';
 
 @Component({
   selector: 'app-contact',
@@ -13,11 +14,14 @@ export class ContactComponent implements OnInit {
   submitted : boolean = false;
   success : boolean = false;
   result : string;
+  modelkontak : Kontak;
+  kontak_arr : Kontak[] = [];
 
   constructor(private formBuilder : FormBuilder, private data : DataService) { 
 
+
     this.messageForm = this.formBuilder.group({
-      name : ['',Validators.required],
+      name : ['Muhammad',[Validators.required,Validators.minLength(10)]],
       phone : ['',Validators.required],
       message : ['',Validators.required]
     })
@@ -29,8 +33,15 @@ export class ContactComponent implements OnInit {
     if(this.messageForm.invalid){
       return;
     }
-
+ 
     this.success = true;
+    
+    //list kontak yg di submit
+    this.modelkontak = new Kontak(this.messageForm.controls.name.value,this.messageForm.controls.phone.value,
+    this.messageForm.controls.message.value);
+    this.kontak_arr.push(this.modelkontak);
+    
+    //call dataservice send SMS
     this.data.SendSMSMessage(this.messageForm.controls.name.value,
       this.messageForm.controls.phone.value,
       this.messageForm.controls.message.value).subscribe(data=>{
---
+++ src/app/kontak.ts
@@ -0,0 +1,8 @@
+export class Kontak {
+
+    constructor(
+        public name: string,
+        public phone: string,
+        public message: string){}
+
+}
